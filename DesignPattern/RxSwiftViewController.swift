6a4a19287b9db6be217cf582cb87933ebb4904e1
chanho lee <leech3878@naver.com>
2023-12-12 16:07:02 +0900

    rxSwift, rxCocoa를 이용한 MVVM패턴 구현 및 파일 이동

--- DesignPattern/RxSwiftViewController.swift
+++ DesignPattern/RxSwiftViewController.swift
@@ -6,10 +6,15 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class RxSwiftViewController: UIViewController {
 
     private let rxView = MVVMView()
+    private let rxViewModel = RxViewModel()
+    private let disposeBag = DisposeBag()
+    
     
     override func loadView() {
         view = rxView
@@ -17,7 +22,30 @@ class RxSwiftViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setInitialData()
+        bind()
+    }
+    
+    private func setInitialData() {
+        rxView.nameLabel.text = humanData[0].name
+        rxView.ageLabel.text = String(humanData[0].age)
+        
+    }
+    
+    private func bind() {
+        let input = RxViewModel.Input(
+            nextButtonClick: rxView.nextButton.rx.tap,
+            previousButtonClick: rxView.previousButton.rx.tap
+        )
+        let output = rxViewModel.transform(input: input)
+        
+        output.nameText
+            .drive(rxView.nameLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        output.ageText
+            .drive(rxView.ageLabel.rx.text)
+            .disposed(by: disposeBag)
     }
 
 }
---
+++ DesignPattern/RxViewModel.swift
@@ -0,0 +1,66 @@
+//
+//  RxViewModel.swift
+//  DesignPattern
+//
+//  Created by t2023-m079 on 12/8/23.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+protocol ViewModelType{
+    associatedtype Input
+    associatedtype Output
+    
+    var disposeBag: DisposeBag { get set }
+    
+    func transform(input: Input) -> Output
+}
+
+final class RxViewModel: ViewModelType {
+    var index: Int = 0
+    
+    struct Input{
+        let nextButtonClick: ControlEvent<Void>
+        let previousButtonClick: ControlEvent<Void>
+    }
+    
+    struct Output{
+        let nameText: Driver<String>
+        let ageText: Driver<String>
+    }
+    
+    var disposeBag: DisposeBag = DisposeBag()
+    
+    func transform(input: Input) -> Output {
+        
+        let nextIndex = input.nextButtonClick
+            .map {
+                self.index += self.index + 1 < humanData.count ? 1 : 0
+                return self.index
+            }
+            .asDriver(onErrorJustReturn: 0)
+        
+        let previousIndex = input.previousButtonClick
+            .map {
+                self.index -= self.index - 1 >= 0 ? 1 : 0
+                return self.index
+            }
+            .asDriver(onErrorJustReturn: 0)
+        
+        let nameText = Driver.merge(nextIndex, previousIndex)
+            .map {
+                humanData.indices.contains($0) ? humanData[$0].name : ""
+            }
+            .asDriver(onErrorJustReturn: "")
+        
+        let ageText = Driver.merge(nextIndex, previousIndex)
+            .map {
+                humanData.indices.contains($0) ? String(humanData[$0].age) : ""
+            }
+            .asDriver(onErrorJustReturn: "")
+
+        return Output(nameText: nameText, ageText: ageText)
+    }
+}
